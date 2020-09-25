35b6897e43275795e702a232eb39e04dd6c8ffef
Nahuel Krowicki <nkrowicki@nubedi.com>
2020-09-25 17:48:13 -0300

    Add spinner

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,43 +1,48 @@
-import React, { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Switch, Redirect } from 'react-router-dom'
-import PublicRoute from './PublicRoute'
-import AuthRouter from './AuthRouter'
-import PrivateRoute from './PrivateRoute'
-import ChecheffScreen from '../components/checheff/ChecheffScreen'
-import { useDispatch } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
+import PublicRoute from "./PublicRoute";
+import AuthRouter from "./AuthRouter";
+import PrivateRoute from "./PrivateRoute";
+import ChecheffScreen from "../components/checheff/ChecheffScreen";
+import { useDispatch } from "react-redux";
+import Spinner from "../components/spinner/Spinner";
+
+import styles from "./AppRouter.module.scss";
 
 const AppRouter = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const [checking, setChecking] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    const [checking, setChecking] = useState(false);
-    const [isLoggedIn, setIsLoggedIn] = useState(true);
-
-
-    if(checking){
-        return (<h1>Wait...</h1>)
-    }
-
+  if (checking) {
     return (
-        <Router>
-            <div>
-                <Switch>
-                    <PublicRoute
-                        path="/auth"
-                        component={AuthRouter}
-                        isAuthenticated={isLoggedIn} />
-                    <PrivateRoute
-                        exact 
-                        path="/"
-                        component={ChecheffScreen}
-                        isAuthenticated={isLoggedIn} />
-                    <Redirect
-                        to={'/auth/login'}
-                        /> 
-                </Switch>
-            </div>
-        </Router>
-    )
-}
+      <div className={styles.center}>
+        <Spinner />
+      </div>
+    );
+  }
 
-export default AppRouter
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <PublicRoute
+            path="/auth"
+            component={AuthRouter}
+            isAuthenticated={isLoggedIn}
+          />
+          <PrivateRoute
+            exact
+            path="/"
+            component={ChecheffScreen}
+            isAuthenticated={isLoggedIn}
+          />
+          <Redirect to={"/auth/login"} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default AppRouter;
