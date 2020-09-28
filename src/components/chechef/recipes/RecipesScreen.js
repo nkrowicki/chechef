2e500bdaaec1f9743ebe1e38ef01a9f603cc567b
Nahuel Krowicki <nkrowicki@nubedi.com>
2020-09-27 23:37:51 -0300

    Working in recipes

--- src/components/chechef/recipes/RecipesScreen.js
+++ src/components/chechef/recipes/RecipesScreen.js
@@ -1,12 +1,53 @@
-import React from 'react';
-// import styles from './RecipesScreen.module.scss';
+import React from "react";
+import styles from './RecipesScreen.module.scss';
 
 const RecipesScreen = () => {
-    return (
-        <h1>
-           Recetas 
-        </h1>
-    )
-}
+  //Load recipes (test in redux with array?)
 
-export default RecipesScreen
+  return (
+    <div id="aspect-content">
+      <div className={styles.aspectTab}>
+        <input
+          id="item-14"
+          type="checkbox"
+          className={styles.aspectInput}
+          name="aspect"
+        />
+        <label htmlFor="item-14" className={styles.aspectLabel} />
+        <div className={styles.aspectContent}>
+          <div className={styles.aspectInfo}>
+            <span className={styles.aspectName}>Receta titulo?</span>
+          </div>
+          {/* <div className={styles.aspectStat}>
+            <div>
+              <span className={styles.negativeCount}>5</span>
+            </div>
+          </div> */}
+        </div>
+        <div className={styles.aspectTabContent}>
+          <div className={styles.sentimentWrapper}>
+            <div>
+              <div>
+                {/* <div className={`${styles.positiveCount} ${styles.opinionHeader}`}>
+                  <span>positive</span>
+                  <span>12</span>
+                </div> */}
+                <div>
+                  <span>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Proin id congue dolor. Vivamus eleifend vitae nunc sed
+                    tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing
+                    elit. Proin id congue dolor. Vivamus eleifend vitae nunc sed
+                    tincidunt.
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RecipesScreen;
